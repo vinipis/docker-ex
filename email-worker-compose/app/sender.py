79c65e76cc22de18cf869b8d934487c8d03f5733
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2018-10-01 09:05:04 -0300

    Ajustes no codigo para recebimento no banco de dados

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -2,7 +2,8 @@ import psycopg2
 import redis
 import json
 import os
-from bottle import route, request
+from bottle import Bottle, request
+
 
 class Sender(Bottle):
     def __init__(self):
@@ -11,13 +12,13 @@ class Sender(Bottle):
         
         redis_host = os.getenv('REDIS_HOST', 'queue')
         self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
-        
+
         db_host = os.getenv('DB_HOST', 'db')
         db_user = os.getenv('DB_USER', 'postgres')
-        dm_name = os.getenv('DB_NAME', 'sender')
+        db_name = os.getenv('DB_NAME', 'sender')
         dsn = f'dbname={db_name} user={db_user} host={db_host}'
         self.conn = psycopg2.connect(dsn)
-
+        
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
         cur = self.conn.cursor()
@@ -28,17 +29,17 @@ class Sender(Bottle):
         msg = {'assunto': assunto, 'mensagem': mensagem}
         self.fila.rpush('sender', json.dumps(msg))
 
-        print('Mensagem registrada com sucesso!')
+        print('Mensagem registrada !')
 
     def send(self):
         assunto = request.forms.get('assunto')
         mensagem = request.forms.get('mensagem')
 
         self.register_message(assunto, mensagem)
-        return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
             assunto, mensagem
         )
 
 if __name__ == '__main__':
     sender = Sender()
-    sender.run(host='0.0.0.0', port=8080, debug=True)
+sender.run(host='0.0.0.0', port=8080, debug=True)
--- email-worker-compose/scripts/init.sql
+++ email-worker-compose/scripts/init.sql
@@ -4,7 +4,7 @@ create database email_sender;
 
 create table emails (
     id serial not null,
-    dtdata timestamp not null default current_timestamp,
+    data timestamp not null default current_timestamp,
     assunto varchar(100) not null,
     mensagem varchar(250) not null
 );
